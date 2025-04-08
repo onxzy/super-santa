7e5cf1b587c0346af374f5fc4083986d3820cb29
Noah Balencourt <noah.balencourt@student-cs.fr>
2025-04-08 10:48:09 +0200

    police

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,15 @@
 'use client'
-import { Inter } from 'next/font/google';
 
 import { useRouter } from 'next/navigation'
 import PrimaryButton from '@/components/PrimaryButton';
+
 
 export default function Home() {
   const router = useRouter();
   return (
     <div>
       <h1>Rudolphe</h1>
-      <p className=''>Page d'accueil</p>
+      <p>Page d'accueil</p>
       <PrimaryButton
         text="Créer un nouveau groupe"
         onClick={() => router.push('/new-group')}
